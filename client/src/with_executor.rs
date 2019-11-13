89a7904cf733247e9ca03208a81fc98ef91ad25a
Thomas Scholtes <thomas@monadic.xyz>
2019-11-13 18:00:19 +0100

    Replace SyncClient with ClientWithExecutor
    We replace `SyncClient` with `ClientWithExecutor`. Instead of returning
    results synchronously like `SyncClient`, `ClientWithExecutor` returns
    futures. But these futures are spawned in an executor that is owned by
    the client. This means that `ClientWithExecutor` can still be used in
    the proxy.
    
    The advantage of the change is that `ClientWithExecutor` unlike
    `SyncClient` implements the `Client` trait. This enables us to make the
    app proxy generic over the client.

---
+++ client/src/with_executor.rs
@@ -0,0 +1,94 @@
+//! Provides [ClientT] that uses owned [Future] executor.
+use futures01::prelude::*;
+
+use crate::*;
+
+/// An adapter for [Client] that implements [ClientT] and spawns all futures in an owned executor.
+///
+/// Makes it possible to call `.wait()` when [Future] based API are not supported.
+pub struct ClientWithExecutor {
+    client: Client,
+    runtime: tokio::runtime::Runtime,
+}
+
+impl ClientWithExecutor {
+    pub fn create() -> Result<Self, Error> {
+        let runtime = tokio::runtime::Runtime::new().unwrap();
+        let client = run_sync(&runtime, Client::create())?;
+        Ok(ClientWithExecutor { client, runtime })
+    }
+
+    fn run_sync<T, F>(&self, f: impl FnOnce(&Client) -> F) -> Response<T, Error>
+    where
+        F: Future<Item = T, Error = Error> + Send + 'static,
+        T: Send + 'static,
+    {
+        Box::new(run_sync(&self.runtime, f(&self.client)).into_future())
+    }
+}
+
+impl ClientT for ClientWithExecutor {
+    fn transfer(
+        &self,
+        key_pair: &ed25519::Pair,
+        receiver: &AccountId,
+        balance: Balance,
+    ) -> Response<(), Error> {
+        self.run_sync(move |client| client.transfer(key_pair, receiver, balance))
+    }
+
+    fn free_balance(&self, account_id: &AccountId) -> Response<Balance, Error> {
+        self.run_sync(move |client| client.free_balance(account_id))
+    }
+
+    fn register_project(
+        &self,
+        author: &ed25519::Pair,
+        project_params: RegisterProjectParams,
+    ) -> Response<(), Error> {
+        self.run_sync(move |client| client.register_project(author, project_params))
+    }
+
+    fn get_project(&self, id: ProjectId) -> Response<Option<Project>, Error> {
+        self.run_sync(move |client| client.get_project(id))
+    }
+
+    fn list_projects(&self) -> Response<Vec<ProjectId>, Error> {
+        self.run_sync(move |client| client.list_projects())
+    }
+
+    fn create_checkpoint(
+        &self,
+        author: &ed25519::Pair,
+        project_hash: H256,
+        prev_cp: Option<CheckpointId>,
+    ) -> Response<CheckpointId, Error> {
+        self.run_sync(move |client| client.create_checkpoint(author, project_hash, prev_cp))
+    }
+
+    fn get_checkpoint(&self, id: CheckpointId) -> Response<Option<Checkpoint>, Error> {
+        self.run_sync(move |client| client.get_checkpoint(id))
+    }
+}
+
+/// Spawn the future in the given runtime and wait for the result.
+fn run_sync<T, E>(
+    runtime: &tokio::runtime::Runtime,
+    f: impl Future<Item = T, Error = E> + Send + 'static,
+) -> Result<T, E>
+where
+    T: Send + 'static,
+    E: Send + 'static,
+{
+    let (sender, receiver) = futures01::sync::oneshot::channel();
+    runtime.executor().spawn(f.then(|res| {
+        // Ignore errors: We don’t care if the receiver was dropped
+        sender.send(res).map_err(|_| ())
+    }));
+    receiver
+        .then(|res| match res {
+            Ok(value) => value,
+            Err(_err) => panic!("ClientWithExecutor: sender was dropped"),
+        })
+        .wait()
+}
--- client/tests/end_to_end.rs
+++ client/tests/end_to_end.rs
@@ -2,19 +2,21 @@
 //!
 //! Note that chain state is shared between the test runs.
 
+use futures01::future::Future as _;
 use radicle_registry_client::{
-    ed25519, Checkpoint, CryptoPair, RegisterProjectParams, SyncClient, H256,
+    ed25519, Checkpoint, ClientT as _, ClientWithExecutor, CryptoPair, RegisterProjectParams, H256,
 };
 
 #[test]
 fn register_project() {
     env_logger::init();
-    let client = SyncClient::create().unwrap();
+    let client = ClientWithExecutor::create().unwrap();
     let alice = ed25519::Pair::from_string("//Alice", None).unwrap();
 
     let project_hash = H256::random();
     let checkpoint_id = client
         .create_checkpoint(&alice, project_hash, None)
+        .wait()
         .unwrap();
     let project_id = ("NAME".to_string(), "DOMAIN".to_string());
     client
@@ -27,15 +29,24 @@ fn register_project() {
                 checkpoint_id,
             },
         )
+        .wait()
         .unwrap();
 
-    let project = client.get_project(project_id.clone()).unwrap().unwrap();
+    let project = client
+        .get_project(project_id.clone())
+        .wait()
+        .unwrap()
+        .unwrap();
     assert_eq!(project.id, ("NAME".to_string(), "DOMAIN".to_string()));
     assert_eq!(project.description, "DESCRIPTION");
     assert_eq!(project.img_url, "IMG_URL");
     assert_eq!(project.current_cp, checkpoint_id);
 
-    let checkpoint = client.get_checkpoint(checkpoint_id).unwrap().unwrap();
+    let checkpoint = client
+        .get_checkpoint(checkpoint_id)
+        .wait()
+        .unwrap()
+        .unwrap();
     let checkpoint_ = Checkpoint {
         parent: None,
         hash: project_hash,
@@ -44,6 +55,7 @@ fn register_project() {
 
     let has_project = client
         .list_projects()
+        .wait()
         .unwrap()
         .iter()
         .any(|id| *id == project_id.clone());
