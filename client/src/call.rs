9deb9be8ab3309307db3733c88652e1cf922c155
Alexandre Baldé (Ubuntu 18.04 WSL) <alexandrer_b@outlook.com>
2019-12-08 01:30:53 +0000

    WIP

--- client/src/call.rs
+++ client/src/call.rs
@@ -1,6 +1,5 @@
 //! Defines [Call] trait and implementations for all transaction parameters.
 
-use frame_support::weights::DispatchInfo;
 use radicle_registry_runtime::{balances, registry, Call as RuntimeCall, Event};
 use sr_primitives::DispatchError;
 
@@ -30,7 +29,7 @@ impl Call for registry::RegisterProjectParams {
     fn result_from_events(events: Vec<Event>) -> Result<Self::Result, EventParseError> {
         let dispatch_result = get_dispatch_result(&events)?;
         match dispatch_result {
-            Ok(_dispatch_info) => find_event(&events, "ProjectRegistered", |event| match event {
+            Ok(()) => find_event(&events, "ProjectRegistered", |event| match event {
                 Event::registry(registry::Event::ProjectRegistered(_project_id, _account_id)) => {
                     Some(Ok(()))
                 }
@@ -51,7 +50,7 @@ impl Call for registry::CreateCheckpointParams {
     fn result_from_events(events: Vec<Event>) -> Result<Self::Result, EventParseError> {
         let dispatch_result = get_dispatch_result(&events)?;
         match dispatch_result {
-            Ok(_dispatch_info) => find_event(&events, "CheckpointCreated", |event| match event {
+            Ok(()) => find_event(&events, "CheckpointCreated", |event| match event {
                 Event::registry(registry::Event::CheckpointCreated(_checkpoint_id)) => Some(Ok(())),
                 _ => None,
             }),
@@ -70,7 +69,7 @@ impl Call for registry::SetCheckpointParams {
     fn result_from_events(events: Vec<Event>) -> Result<Self::Result, EventParseError> {
         let dispatch_result = get_dispatch_result(&events)?;
         match dispatch_result {
-            Ok(_dispatch_info) => find_event(&events, "CheckpointSet", |event| match event {
+            Ok(()) => find_event(&events, "CheckpointSet", |event| match event {
                 Event::registry(registry::Event::CheckpointSet(_project_id, _checkpoint_id)) => {
                     Some(Ok(()))
                 }
@@ -90,9 +89,7 @@ impl Call for crate::TransferParams {
 
     fn result_from_events(events: Vec<Event>) -> Result<Self::Result, EventParseError> {
         let dispatch_result = get_dispatch_result(&events)?;
-        Ok(dispatch_result
-            .map_err(|dispatch_error| dispatch_error.message)
-            .map(|_| ()))
+        Ok(dispatch_result.map_err(|dispatch_error| dispatch_error.message))
     }
 
     fn into_runtime_call(self) -> RuntimeCall {
@@ -105,9 +102,7 @@ impl Call for registry::TransferFromProjectParams {
 
     fn result_from_events(events: Vec<Event>) -> Result<Self::Result, EventParseError> {
         let dispatch_result = get_dispatch_result(&events)?;
-        Ok(dispatch_result
-            .map_err(|dispatch_error| dispatch_error.message)
-            .map(|_| ()))
+        Ok(dispatch_result.map_err(|dispatch_error| dispatch_error.message))
     }
 
     fn into_runtime_call(self) -> RuntimeCall {
@@ -123,12 +118,10 @@ impl Call for registry::TransferFromProjectParams {
 /// Returns an outer [EventParseError] if no [frame_system::Event] was present in `events`.
 ///
 /// Because of an issue with substrate the `message` field of [DispatchError] will always be `None`
-fn get_dispatch_result(
-    events: &[Event],
-) -> Result<Result<DispatchInfo, DispatchError>, EventParseError> {
+fn get_dispatch_result(events: &[Event]) -> Result<Result<(), DispatchError>, EventParseError> {
     find_event(events, "System", |event| match event {
         Event::system(system_event) => match system_event {
-            frame_system::Event::ExtrinsicSuccess(ref dispatch_info) => Some(Ok(*dispatch_info)),
+            frame_system::Event::ExtrinsicSuccess(_) => Some(Ok(())),
             frame_system::Event::ExtrinsicFailed(ref dispatch_error, _) => {
                 Some(Err(*dispatch_error))
             }
