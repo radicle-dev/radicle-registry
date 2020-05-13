9de82f38c3385c9041d9a02ed9ce2677f4558f32
Nuno Alexandre <hi@nunoalexandre.com>
2020-05-13 10:18:05 +0200

    cli: Nit improve the key_pair_storage
    * Re-order the code in a more sensible way, having the fundamental types
    on top, followed by the pub functions, and finally the private ones.
    * Improve and fix some docs

--- cli/src/key_pair_storage.rs
+++ cli/src/key_pair_storage.rs
@@ -22,8 +22,35 @@ use std::collections::HashMap;
 use std::fs::File;
 use thiserror::Error as ThisError;
 
+use lazy_static::lazy_static;
 use std::io::Error as IOError;
 use std::path::{Path, PathBuf};
+
+lazy_static! {
+    /// The file where the key pairs are stored.
+    static ref FILE: PathBuf = build_path("key-pairs.json");
+}
+
+/// The possible file variants to be handled when deserializing FILE.
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
+enum KeyStorageFile {
+    /// The genesis, unversioned file variant.
+    Unversioned(HashMap<String, KeyPairData>),
+
+    /// A versioned file variant, to which we have moved to
+    /// in order to leverage backwards-compatibility.
+    Versioned(VersionedFile),
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(tag = "version")]
+enum VersionedFile {
+    #[serde(rename = "1")]
+    V1 {
+        key_pairs: HashMap<String, KeyPairData>,
+    },
+}
 
 /// The data that is stored in the filesystem relative
 /// to a key pair. The name of the key pair is used as
@@ -65,11 +92,10 @@ pub enum Error {
 }
 
 fn io_error_message(action: &str) -> String {
-    let path = build_path(FILE);
     format!(
         "Failed to {} the key-pairs file: '{}'",
         action,
-        path.display()
+        FILE.display()
     )
 }
 
@@ -93,6 +119,20 @@ pub enum ReadingError {
     Deserialization(serde_json::Error),
 }
 
+/// List all the stored key pairs.
+///
+/// Preemptively [init()]s the storage on disk and checks permissions.
+/// It can fail from IO errors or Serde Json errors.
+pub fn list() -> Result<HashMap<String, KeyPairData>, Error> {
+    use {KeyStorageFile::*, VersionedFile::*};
+
+    init()?;
+    match parse_file()? {
+        Unversioned(key_pairs) => Ok(key_pairs),
+        Versioned(V1 { key_pairs }) => Ok(key_pairs),
+    }
+}
+
 /// Add a key pair to the storage.
 ///
 /// Fails if a key pair with the given `name` already exists.
@@ -107,20 +147,6 @@ pub fn add(name: String, data: KeyPairData) -> Result<(), Error> {
     update(key_pairs)
 }
 
-/// List all the stored key-pairs.
-///
-/// It can fail from IO errors or Serde Json errors.
-/// Attempts to migrate the key-pairs file if outdated.
-pub fn list() -> Result<HashMap<String, KeyPairData>, Error> {
-    use {KeyStorageFile::*, VersionedFile::*};
-
-    init()?;
-    match parse_file()? {
-        Unversioned(key_pairs) => Ok(key_pairs),
-        Versioned(V1 { key_pairs }) => Ok(key_pairs),
-    }
-}
-
 /// Get a key pair by name.
 ///
 /// It can fail from IO and Serde Json errors, or if no such
@@ -131,14 +157,15 @@ pub fn get(name: &str) -> Result<KeyPairData, Error> {
 
 fn update(key_pairs: HashMap<String, KeyPairData>) -> Result<(), Error> {
     let data = VersionedFile::V1 { key_pairs };
-    let path_buf = build_path(FILE);
     let new_content = serde_json::to_string_pretty(&data).map_err(WritingError::Serialization)?;
-    std::fs::write(path_buf.as_path(), new_content.as_bytes()).map_err(WritingError::IO)?;
+    std::fs::write(FILE.as_path(), new_content.as_bytes()).map_err(WritingError::IO)?;
     Ok(())
 }
 
-/// The file where the user key-pairs are stored.
-const FILE: &str = "key-pairs.json";
+fn parse_file() -> Result<KeyStorageFile, Error> {
+    let file = File::open(FILE.as_path()).map_err(ReadingError::IO)?;
+    serde_json::from_reader(&file).map_err(|e| ReadingError::Deserialization(e).into())
+}
 
 /// Build the path to the given filename under [dir()].
 fn build_path(filename: &str) -> PathBuf {
@@ -152,62 +179,33 @@ fn dir() -> PathBuf {
         .join("radicle-registry-cli")
 }
 
-fn parse_file() -> Result<KeyStorageFile, Error> {
-    let path_buf = build_path(FILE);
-    let file = File::open(path_buf.as_path()).map_err(ReadingError::IO)?;
-
-    serde_json::from_reader(&file).map_err(|e| ReadingError::Deserialization(e).into())
-}
-
-/// The possible file variants to be handled when deserialization [FILE].
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(untagged)]
-enum KeyStorageFile {
-    /// The genesis, unversioned file variant.
-    Unversioned(HashMap<String, KeyPairData>),
-
-    /// A versioned file variant, to which we have moved to
-    /// in order to leverage backwards-compatibility.
-    Versioned(VersionedFile),
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-#[serde(tag = "version")]
-enum VersionedFile {
-    #[serde(rename = "1")]
-    V1 {
-        key_pairs: HashMap<String, KeyPairData>,
-    },
-}
-
 /// Initialize the storage on disk to be used correctly.
 ///   * Create the directory structure and check permissions
-///   * Create and initialize the [FILE] where the key pairs will be stored.
+///   * Create and initialize the FILE where the key pairs will be stored.
 fn init() -> Result<(), Error> {
-    let path_buf = build_path(FILE);
-    let path = path_buf.as_path();
-
-    init_dir(path.parent().unwrap().to_path_buf())?;
-    init_file(path)?;
+    let dir = FILE.parent().unwrap();
+    init_dir(&dir)?;
+    init_file(&FILE)?;
     Ok(())
 }
 
-/// Ensure that the given directory path is ready to be used.
+/// Initialize the given directory to be used.
 /// Fails with
 ///   * [Error::CannotCreateDirectory] if the directory
 ///     does not exist and fails to be created.
 ///   * [Error::CannotReadDirectory] if the directory
-///     does exist but can not be read.
-fn init_dir(dir: PathBuf) -> Result<PathBuf, Error> {
-    std::fs::create_dir_all(&dir).map_err(|err| Error::CannotCreateDirectory(err, dir.clone()))?;
-    File::open(&dir).map_err(|err| Error::CannotReadDirectory(err, dir.clone()))?;
-    Ok(dir)
+///     does exist but cannot be read.
+fn init_dir(dir: &Path) -> Result<(), Error> {
+    std::fs::create_dir_all(&dir)
+        .map_err(|err| Error::CannotCreateDirectory(err, dir.to_path_buf()))?;
+    File::open(dir).map_err(|err| Error::CannotReadDirectory(err, dir.to_path_buf()))?;
+    Ok(())
 }
 
 /// Init the key-pair storage file on disk.
 ///
-/// Renames the legacy `accounts.json` file to [FILE] and creates
-/// [FILE] an empty value to be deserializable if it doesn't exist.
+///   * Rename the legacy `accounts.json` file to the FILE name.
+///   * Create FILE if it doesn't yet exist.
 fn init_file(path: &Path) -> Result<(), Error> {
     if !path.exists() {
         let old_path = build_path("accounts.json");
