7e0859964e3d4ed6aebac9eb1125068621457cf4
Thomas Scholtes <thomas@monadic.xyz>
2020-06-11 10:47:35 +0200

    Require version in transaction signature
    Fixes #471

--- client/src/transaction.rs
+++ client/src/transaction.rs
@@ -98,6 +98,7 @@ fn transaction_extra_to_runtime_extra(
     SignedExtra,
     <SignedExtra as SignedExtension>::AdditionalSigned,
 ) {
+    let check_version = frame_system::CheckVersion::new();
     let check_genesis = frame_system::CheckGenesis::new();
     let check_era = frame_system::CheckEra::from(Era::Immortal);
     let check_nonce = frame_system::CheckNonce::from(extra.nonce);
@@ -105,6 +106,9 @@ fn transaction_extra_to_runtime_extra(
     let pay_tx_fee = PayTxFee { fee: extra.fee };
 
     let additional_signed = (
+        check_version
+            .additional_signed()
+            .expect("statically guaranteed to always return ok"),
         // Genesis hash
         extra.genesis_hash,
         // Era
@@ -121,6 +125,7 @@ fn transaction_extra_to_runtime_extra(
     );
 
     let extra = (
+        check_version,
         check_genesis,
         check_era,
         check_nonce,
